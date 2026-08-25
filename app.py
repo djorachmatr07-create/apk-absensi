6d670c7871fd3ae2e9f186be6e6c25292d2ebf2a
djorachmatr07-create <djorachmatr07@gmail.com>
2026-08-25 13:24:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ try:
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    
-    # Biar kita tau file apa aja yg bisa diakses
-    files = client.list_spreadsheet_files()
-    st.write("File yg kebaca:", [f['name'] for f in files])
-    
-    sheet = client.open("REKAP").sheet1
-    
+    sheet = client.open("REKAP").worksheet("REKAP") # INI YG DIGANTI
+    st.success("✅ Konek ke Google Sheet Berhasil")
 except Exception as e:
     st.error(f"Gagal konek: {e}")
     st.stop()
+
+id_karyawan = st.text_input("Masukkan ID Karyawan")
+if st.button("Absen Masuk"):
+    if id_karyawan:
+        waktu = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        sheet.append_row([id_karyawan, waktu, "", "", "", "", ""])
+        st.success(f"Absen Masuk Berhasil jam {waktu}")
