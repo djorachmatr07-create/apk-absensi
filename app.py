aa090a8a98e28c0877e626fa0dd8aff7e9793f44
djorachmatr07-create <djorachmatr07@gmail.com>
2026-08-25 13:30:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ try:
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    sheet = client.open("REKAP").worksheet("REKAP") # INI YG DIGANTI
+    
+    # PAKE ID SHEET BIAR GAK SALAH NAMA
+    SHEET_ID = "https://docs.google.com/spreadsheets/d/1ZNPyue3P6RIX6JXdQW_sFjI9Faq0bEIXBM8i49E_5qQ/edit?usp=drivesdk" 
+    sheet = client.open_by_key(SHEET_ID).sheet1
+    
     st.success("✅ Konek ke Google Sheet Berhasil")
 except Exception as e:
     st.error(f"Gagal konek: {e}")
@@ -20,5 +24,5 @@ id_karyawan = st.text_input("Masukkan ID Karyawan")
 if st.button("Absen Masuk"):
     if id_karyawan:
         waktu = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        sheet.append_row([id_karyawan, waktu, "", "", "", "", ""])
+        sheet.append_row([id_karyawan, waktu, "", "", "", "", "", ""])
         st.success(f"Absen Masuk Berhasil jam {waktu}")
